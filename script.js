ddd09e3d643a0f33d5e76d9a1f2925cbc4994f22
prestf8 <prestf8@gmail.com>
2021-07-24 17:42:14 -0700

    added bugged operator functionality

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let displayValue = "";
 let operatorStage = false;
 
 const numberKeys = Array.from(document.getElementsByClassName("number-key"));
+const clearKey = document.getElementById("clear");
+const operatorKeys = Array.from(
+  document.getElementsByClassName("operator-key")
+);
+
 numberKeys.forEach((numberKey) =>
   numberKey.addEventListener("click", function (e) {
     if (operatorStage) {
@@ -15,9 +20,37 @@ numberKeys.forEach((numberKey) =>
       operatorStage = false;
     }
 
-    if (displayValue.length <= 11) {
+    if (displayValue.length <= 9) {
       displayValue += e.target.textContent;
       updateDisplay();
+    }
+  })
+);
+
+clearKey.addEventListener("click", function () {
+  operatorStage = false;
+  displayValue = "";
+  updateDisplay();
+  currentOperation.firstOperand = "";
+  currentOperation.operator = "";
+});
+
+operatorKeys.forEach((operatorKey) =>
+  operatorKey.addEventListener("click", function () {
+    if (!displayValue) return;
+
+    currentOperation.operator = operatorKeys.textContent; // all of the cases below update operator
+
+    if (currentOperation.operator && operatorStage) {
+      return;
+    }
+
+    operatorStage = true;
+
+    if (!currentOperation.firstOperand) {
+      currentOperation.firstOperand = displayValue;
+    } else {
+      setupOperation();
     }
   })
 );
@@ -25,4 +58,41 @@ numberKeys.forEach((numberKey) =>
 function updateDisplay() {
   const display = document.getElementById("display");
   display.textContent = displayValue;
+}
+
+function setupOperation() {
+  currentOperation.secondOperand = displayValue;
+  currentOperation.firstOperand = operate();
+  displayValue = currentOperation.firstOperand;
+  currentOperation.secondOperand = "";
+}
+
+function operate() {
+  switch (currentOperation.operator) {
+    case "+":
+      return add();
+      break;
+    case "-":
+      return subtract();
+      break;
+    case "/":
+      return divide();
+      break;
+    case "*":
+      return multiply();
+      break;
+  }
+}
+
+function add() {
+  return currentOperation.firstOperand + currentOperation.secondOperand;
+}
+function subtract() {
+  return currentOperation.firstOperand - currentOperation.secondOperand;
+}
+function divide() {
+  return currentOperation.firstOperand / currentOperation.secondOperand;
+}
+function multiply() {
+  return currentOperation.firstOperand * currentOperation.secondOperand;
 }

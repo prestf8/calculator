4e539ca61abaceb2cc459786738867aab75530bf
prestf8 <prestf8@gmail.com>
2021-07-24 17:24:16 -0700

    number keys function and display updates

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let currentOperation = {
-    firstOperand: "",
-    secondOperand: "",
-    operator: ""
-}
+  firstOperand: "",
+  secondOperand: "",
+  operator: "",
+};
 
 let displayValue = "";
 let operatorStage = false;
 
+const numberKeys = Array.from(document.getElementsByClassName("number-key"));
+numberKeys.forEach((numberKey) =>
+  numberKey.addEventListener("click", function (e) {
+    if (operatorStage) {
+      displayValue = "";
+      operatorStage = false;
+    }
 
+    if (displayValue.length <= 11) {
+      displayValue += e.target.textContent;
+      updateDisplay();
+    }
+  })
+);
 
+function updateDisplay() {
+  const display = document.getElementById("display");
+  display.textContent = displayValue;
+}

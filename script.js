01ad5b85e64677ef48e5465b9355c01c49360128
prestf8 <prestf8@gmail.com>
2021-07-24 23:26:18 -0700

    equals button added and functionality works

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const clearKey = document.getElementById("clear");
 const operatorKeys = Array.from(
   document.getElementsByClassName("operator-key")
 );
+const equalsKey = document.getElementById("equals");
 
 numberKeys.forEach((numberKey) =>
   numberKey.addEventListener("click", function (e) {
@@ -24,6 +25,8 @@ numberKeys.forEach((numberKey) =>
       displayValue += e.target.textContent;
       updateDisplay();
     }
+    console.log(displayValue);
+    console.log(currentOperation);
   })
 );
 
@@ -50,12 +53,32 @@ operatorKeys.forEach((operatorKey) =>
     if (!currentOperation.firstOperand) {
       currentOperation.firstOperand = displayValue;
       currentOperation.operator = operator;
+    } else if (currentOperation.firstOperand && !currentOperation.operator) {
+      // for the case when user presses operator after equals key
+      currentOperation.firstOperand = displayValue;
+      currentOperation.operator = operator;
     } else {
       setupOperation();
       currentOperation.operator = operator;
     }
   })
 );
+
+// WHENEVER equal Operator button is pressed:
+// 	IF FirstOperand is none: RETURN
+// 	ELSE:
+// 		SetupOperation()
+// 		Operator <- ""
+// 	ENDIF
+// ENDFUNCTION
+
+equalsKey.addEventListener("click", function () {
+  if (!currentOperation.firstOperand) return;
+  setupOperation();
+  currentOperation.operator = "";
+  console.log(currentOperation);
+  console.log(displayValue);
+});
 
 function updateDisplay() {
   const display = document.getElementById("display");
@@ -96,14 +119,14 @@ function subtract() {
   );
 }
 function divide() {
-  return (
+  return Math.round(
     parseFloat(currentOperation.firstOperand) /
-    parseFloat(currentOperation.secondOperand)
+      parseFloat(currentOperation.secondOperand)
   );
 }
 function multiply() {
-  return (
+  return Math.round(
     parseFloat(currentOperation.firstOperand) *
-    parseFloat(currentOperation.secondOperand)
+      parseFloat(currentOperation.secondOperand)
   );
 }
